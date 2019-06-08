2639711e302bd49cb6df3b6ea1acc2344a5878e6
wikikiy <45632543+wikikiy@users.noreply.github.com>
2019-06-08 16:30:34 +0800

    Update Image.py

--- Image.py
+++ Image.py
@@ -1,9 +1,7 @@
 import requests
 from fake_useragent import UserAgent
 from lxml import etree
-import time
 
-# Get first page urls
 def get_url(url):
     response=requests.get(url,headers={"User-Agent": UserAgent(verify_ssl=False).random}).text
     e=etree.HTML(response)
@@ -16,7 +14,6 @@ def get_url(url):
         urls.append(last_url)
     return urls
 
-# Get all image url
 def parse_url(pages):
     all_url=[]
     for page in pages:
@@ -26,27 +23,31 @@ def parse_url(pages):
         num=int(nums[-2])
         new_page = "".join(page).split(".html")[0]
         for i in range(2,num+1):
-            # Split joint all image url
             last_page=new_page+"p"+str(i)+".html"
             all_url.append(last_page)
     return all_url
 
-# Down all image
 def down_img(imgsurl):
+    num = 0
     for imgurl in imgsurl:
         response = requests.get(imgurl,headers={"User-Agent": UserAgent(verify_ssl=False).random}).text
         e = etree.HTML(response)
         imglinks = e.xpath('//div[@id="content"]/div/p/img/@src')
-        for imglink in imglinks:
-            link="".join(imglink).split("../..")[-1]
-            # Get image name
-            path="".join(imglink).split("/")[-1]
-            links="https://www.24fa.top"+link
-            resp=requests.get(links,headers={"User-Agent": UserAgent(verify_ssl=False).random})
-            # Enter your store folder
-            with open("***"+path,"wb+") as f:
-                time.sleep(3)
-                f.write(resp.content)
+        if imglinks!=0:
+            for imglink in imglinks:
+                link="".join(imglink).split("../..")[-1]
+                path="".join(imglink).split("/")[-1]
+                links="https://www.24fa.top"+link
+                resp=requests.get(links,headers={"User-Agent": UserAgent(verify_ssl=False).random})
+                if resp.status_code==200:
+                    with open("D:\\meitu\\"+path,"wb+") as f:
+                        print("正在下载第%s张"%num)
+                        f.write(resp.content)
+                        num+=1
+                else:
+                    print("图片加载失败")
+        else:
+            print("遇到广告页")
 
 if __name__=='__main__':
     url="https://www.24fa.top/MeiNv/"
